5be72ed29acbcc6c0d40d01beb8a4cae28b5c43f
Adriano Castro <adriano.castro9@kit.edu>
2024-03-26 18:52:24 +0000

    docs(usig_msg): enhance comments

--- src/peer_message/usig_message/mod.rs
+++ src/peer_message/usig_message/mod.rs
@@ -1,5 +1,6 @@
 //! Defines a message of type [UsigMessage].
-//! Such messages are either of inner type [ViewPeerMessage], [ViewChangeV], [NewView] or [Checkpoint].
+//! Such messages are either of inner type [ViewPeerMessage], [ViewChangeV],
+//! [NewView] or [Checkpoint].
 //! Moreover, messages of type [UsigMessage] are signed by a USIG.
 //! For further explanation of the inner types, see the specific documentation.
 
@@ -24,24 +25,34 @@ use self::{
     view_peer_message::ViewPeerMessage,
 };
 
-/// A [UsigMessage] is a message that contains a USIG signature,
-/// and is either a [ViewPeerMessage], [ViewChangeV], [NewView] or a [Checkpoint].
+/// A [UsigMessageV] is a message that contains a USIG signature,
+/// and is either a [UsigMessageV::View], [UsigMessageV::ViewChange],
+/// [UsigMessageV::NewView] or a [UsigMessageV::Checkpoint].
+///
+/// A [UsigMessageV::View] should be created when the USIG signed message is
+/// internally a [ViewPeerMessage].
+/// A [UsigMessageV::ViewChange] should be created when the USIG signed message
+/// is internally a [ViewChangeV] message.
+/// A [UsigMessageV::NewView] should be created when the USIG signed message is
+/// internally a [NewView] message.
+/// A [UsigMessageV::Checkpoint] should be created when the USIG signed message
+/// is internally a [Checkpoint] message.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub(crate) enum UsigMessageV<V: ViewChangeVariant<P, Sig>, P, Sig> {
-    /// A [UsigMessage] of type [ViewPeerMessage].
+    /// A [UsigMessageV] of type [ViewPeerMessage].
     View(ViewPeerMessage<P, Sig>),
-    /// A [UsigMessage] of type [ViewChangeV].
+    /// A [UsigMessageV] of type [ViewChangeV].
     ViewChange(ViewChangeV<V, P, Sig>),
-    /// A [UsigMessage] of type [NewView].
+    /// A [UsigMessageV] of type [NewView].
     NewView(NewView<P, Sig>),
-    /// A [UsigMessage] of type [Checkpoint].
+    /// A [UsigMessageV] of type [Checkpoint].
     Checkpoint(Checkpoint<Sig>),
 }
 
 impl<V: ViewChangeVariant<P, Sig>, T: Into<ViewPeerMessage<P, Sig>>, P, Sig> From<T>
     for UsigMessageV<V, P, Sig>
 {
-    /// Create a [UsigMessage] based on a [ViewPeerMessage].
+    /// Create a [UsigMessageV] based on a [ViewPeerMessage].
     fn from(view_peer_message: T) -> Self {
         Self::View(view_peer_message.into())
     }
@@ -49,24 +60,26 @@ impl<V: ViewChangeVariant<P, Sig>, T: Into<ViewPeerMessage<P, Sig>>, P, Sig> Fro
 impl<V: ViewChangeVariant<P, Sig>, P, Sig> From<ViewChangeV<V, P, Sig>>
     for UsigMessageV<V, P, Sig>
 {
-    /// Create a [UsigMessage] based on a [ViewChangeV].
+    /// Create a [UsigMessageV] based on a [ViewChangeV].
     fn from(view_change: ViewChangeV<V, P, Sig>) -> Self {
         Self::ViewChange(view_change)
     }
 }
 impl<V: ViewChangeVariant<P, Sig>, P, Sig> From<NewView<P, Sig>> for UsigMessageV<V, P, Sig> {
-    /// Create a [UsigMessage] based on a [NewView].
+    /// Create a [UsigMessageV] based on a [NewView].
     fn from(new_view: NewView<P, Sig>) -> Self {
         Self::NewView(new_view)
     }
 }
 
-/// A not further specified [UsigMessage] is a [UsigMessage] that contains a log of messages if it is of inner type [ViewChangeV].
+/// A [UsigMessage] is a [UsigMessageV] that contains a log of messages if its
+/// inner type is [ViewChangeV], i.e. a [UsigMessageV] with
+/// [ViewChangeVariantLog].
 pub(crate) type UsigMessage<P, Sig> = UsigMessageV<ViewChangeVariantLog<P, Sig>, P, Sig>;
 
 impl<P: Clone + Serialize, Sig: Clone + Serialize> UsigMessage<P, Sig> {
     /// Convert a [UsigMessage] to the variant with no log
-    /// (only in case of a [ViewChangeV] message).
+    /// (only relevant for when the internal message is of type [ViewChangeV]).
     fn to_no_log(&self) -> UsigMessageV<ViewChangeVariantNoLog, P, Sig> {
         match self {
             UsigMessageV::View(v) => v.clone().into(),
@@ -85,7 +98,8 @@ impl<V: ViewChangeVariant<P, Sig>, P, Sig> From<Checkpoint<Sig>> for UsigMessage
 }
 
 impl<V: ViewChangeVariant<P, Sig>, P, Sig: Counter> Counter for UsigMessageV<V, P, Sig> {
-    /// Returns the counter of the [UsigMessage] by returning the counter of its inner type.
+    /// Returns the counter of the [UsigMessage] by returning the counter of its
+    /// inner type.
     fn counter(&self) -> usig::Count {
         match self {
             Self::View(view) => view.counter(),
@@ -97,8 +111,9 @@ impl<V: ViewChangeVariant<P, Sig>, P, Sig: Counter> Counter for UsigMessageV<V, 
 }
 
 impl<V: ViewChangeVariant<P, Sig>, P, Sig> AsRef<ReplicaId> for UsigMessageV<V, P, Sig> {
-    /// Referencing [UsigMessageV] returns a reference to the origin of the [UsigMessageV]
-    /// (either of the [ViewPeerMessage], [ViewChangeV], [NewView] or [Checkpoint]).
+    /// Referencing [UsigMessageV] returns a reference to the origin of the
+    /// [UsigMessageV] (either of the [ViewPeerMessage], [ViewChangeV],
+    /// [NewView] or [Checkpoint]).
     fn as_ref(&self) -> &ReplicaId {
         match self {
             Self::View(view_peer_message) => view_peer_message.as_ref(),
@@ -111,6 +126,12 @@ impl<V: ViewChangeVariant<P, Sig>, P, Sig> AsRef<ReplicaId> for UsigMessageV<V, 
 
 impl<P: RequestPayload, Sig: Serialize + Counter + Debug> UsigMessage<P, Sig> {
     /// Validates the [UsigMessage] by validating its inner type.
+    ///
+    /// # Arguments
+    ///
+    /// * `config` - The [Config] of the algorithm.
+    /// * `usig` - The USIG signature that should be a valid one for this
+    ///            [UsigMessage] message.
     pub(crate) fn validate(
         &self,
         config: &Config,
