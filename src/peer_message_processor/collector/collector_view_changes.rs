924d87c1da1e22e1e8fdf31cff59e95b29ba2a37
Adriano Castro <adriano.castro9@kit.edu>
2024-04-16 11:48:52 +0000

    refactor(collector_vc): adjust collector of view changes to be a type

--- src/peer_message_processor/collector/collector_view_changes.rs
+++ src/peer_message_processor/collector/collector_view_changes.rs
@@ -2,33 +2,25 @@
 //! After a sufficient amount (t + 1) of ViewChanges are received and collected, the next [View] broadcasts a NewView message.
 //! The Commits must share the same next [crate::View].
 
-use serde::{Deserialize, Serialize};
 use tracing::debug;
 
 use crate::{peer_message::usig_message::view_change::ViewChange, Config, View};
 
 use super::CollectorMessages;
 
-/// The purpose of the struct is to collect messages of type ViewChange.
-/// They are organized by the next [View] that is set in their content.
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub(crate) struct CollectorViewChanges<P, Sig>(CollectorMessages<View, ViewChange<P, Sig>>);
+pub(crate) type CollectorViewChanges<P, Sig> = CollectorMessages<View, ViewChange<P, Sig>>;
 
 impl<P: Clone, Sig: Clone> CollectorViewChanges<P, Sig> {
-    /// Creates a new collector of ViewChanges.
-    pub(crate) fn new() -> Self {
-        Self(CollectorMessages::new())
-    }
     /// Inserts a ViewChange message and returns the amount of so far collected
     /// ViewChanges for the same next [View] as the given message.
-    pub(crate) fn collect(&mut self, msg: ViewChange<P, Sig>, config: &Config) -> u64 {
+    pub(crate) fn collect_view_change(&mut self, msg: ViewChange<P, Sig>) -> u64 {
         let origin = msg.origin;
         let next_view = msg.next_view;
         debug!(
             "Collecting ViewChange (origin: {:?}, next_view: {:?}) ...",
             origin, next_view
         );
-        let amount_collected = self.0.collect(msg, origin, next_view, config);
+        let amount_collected = self.collect(msg, origin, next_view);
         debug!(
             "Successfully collected ViewChange (origin: {:?}, next view: {:?}).",
             origin, next_view
@@ -39,7 +31,7 @@ impl<P: Clone, Sig: Clone> CollectorViewChanges<P, Sig> {
     /// Retrieves a collection of at least t + 1 ViewChanges if they are valid and
     /// if already at least t + 1 ViewChanges have been received for the same next [View].
     /// Is this the case, then the collection only retains ViewChanges which are for a higher next [View].
-    pub(crate) fn retrieve(
+    pub(crate) fn retrieve_collected_view_changes(
         &mut self,
         msg: &ViewChange<P, Sig>,
         config: &Config,
@@ -48,7 +40,7 @@ impl<P: Clone, Sig: Clone> CollectorViewChanges<P, Sig> {
             "Retrieving ViewChanges (next view: {:?}) from collector ...",
             msg.next_view,
         );
-        let mut retrieved = self.0.retrieve(msg.next_view, config)?;
+        let mut retrieved = self.retrieve(msg.next_view, config)?;
         let mut retrieved_single = Vec::new();
         retrieved_single.push(retrieved.0);
         retrieved_single.append(&mut retrieved.1);
