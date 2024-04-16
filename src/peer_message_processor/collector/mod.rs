349e0c1badc433dbc4c4945841607b071f5dc5e3
Adriano Castro <adriano.castro9@kit.edu>
2024-04-16 17:28:19 +0000

    refactor(collectors): remove no longer necessary bool array

--- src/peer_message_processor/collector/mod.rs
+++ src/peer_message_processor/collector/mod.rs
@@ -11,10 +11,10 @@ pub(crate) mod collector_commits;
 pub(crate) mod collector_req_view_changes;
 pub(crate) mod collector_view_changes;
 
-use std::{collections::HashMap, hash::Hash, mem, num::NonZeroU64};
+use std::{collections::HashMap, hash::Hash};
 
 use serde::{Deserialize, Serialize};
-use shared_ids::{AnyId, ReplicaId};
+use shared_ids::ReplicaId;
 use tracing::debug;
 
 use crate::Config;
@@ -90,81 +90,5 @@ impl<K: Eq + Hash + PartialOrd + Clone, M> CollectorMessages<K, M> {
         self.0.retain(|n, _| n > &key);
         let my_retrieved_message = my_retrieved_message?;
         Some((my_retrieved_message, other_retrieved_messages))
-    }
-}
-
-/// Defines a boolean array.
-/// Sets specific entries to true.
-/// Keeps count of how many entries are set to true.
-#[derive(Clone, Default, Debug, Serialize, Deserialize)]
-pub(super) struct BoolArray {
-    // The counter to keep count of how many entries are set to true in the vec.
-    pub(crate) counter: u64,
-    // The vec of bools
-    bools: Vec<bool>,
-}
-
-impl BoolArray {
-    /// Creates a new [BoolArray] of the given size.
-    pub fn new(size: NonZeroU64) -> Self {
-        let mut bools = Vec::with_capacity(size.get() as usize);
-        let mut amount_initialized = 0;
-        while amount_initialized < size.get() {
-            bools.push(false);
-            amount_initialized += 1;
-        }
-        Self { counter: 0, bools }
-    }
-
-    /// Updates the boolean array according to the provided index.
-    /// That is to say, it sets the entry at the index to true
-    /// and increments the counter if the entry was false before.
-    pub(super) fn update(&mut self, index: usize) -> bool {
-        if !self.bools[index] {
-            self.bools[index] = true;
-            self.counter += 1;
-            return true;
-        }
-        false
-    }
-}
-
-/// Keeps track of how many messages have been received and from which replica.
-#[derive(Debug, Clone)]
-pub(crate) struct CollectorBools<K: Eq + Hash + PartialOrd>(HashMap<K, BoolArray>);
-
-impl<K: Eq + Hash + PartialOrd> CollectorBools<K> {
-    /// Creates a new [CollectorBools].
-    pub(crate) fn new() -> Self {
-        Self(HashMap::new())
-    }
-    /// Collects a message by updating the [BoolArray] accordingly and returns the amount of collected messages
-    /// received with the same key as the input.
-    pub(crate) fn collect(&mut self, key: K, from: ReplicaId, config: &Config) -> u64 {
-        let mut bool_array = BoolArray::new(config.n);
-        match self.0.get_mut(&key) {
-            Some(actual_bool_array) => {
-                bool_array = mem::take(actual_bool_array);
-                let index = from.as_u64() as usize;
-                if bool_array.update(index) {
-                    debug!("Inserted message (origin: {from:?}) into collector.");
-                } else {
-                    debug!("Skipped inserting message (origin: {from:?}) into collector: Message was a duplicate.");
-                };
-            }
-            None => {
-                let index = from.as_u64() as usize;
-                assert!(bool_array.update(index));
-                debug!("Inserted message (origin: {from:?}) into collector.");
-            }
-        }
-        let amount_collected = bool_array.counter;
-        self.0.insert(key, bool_array);
-        amount_collected
-    }
-
-    /// Cleans up the collection by retaining only messages with a higher key.
-    pub(crate) fn clean_up(&mut self, key: K) {
-        self.0.retain(|k, _| k > &key);
     }
 }
