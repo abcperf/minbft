b2b402435c5fe3e2874913fddfed8d85de551262
Adriano Castro <adriano.castro9@kit.edu>
2024-04-16 11:39:29 +0000

    refactor(collector_cp): adjust collector of messages

--- src/peer_message_processor/collector/mod.rs
+++ src/peer_message_processor/collector/mod.rs
@@ -21,83 +21,72 @@ use crate::Config;
 
 /// Collects messages and keeps track of how many messages have been received.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub(crate) struct CollectorMessages<K: Eq + Hash + PartialOrd, M>(
-    HashMap<K, (u64, Vec<Option<M>>)>,
+pub(crate) struct CollectorMessages<K: Eq + Hash + PartialOrd + Clone, M>(
+    HashMap<K, HashMap<ReplicaId, M>>,
 );
 
-impl<K: Eq + Hash + PartialOrd, M> CollectorMessages<K, M> {
+impl<K: Eq + Hash + PartialOrd + Clone, M> CollectorMessages<K, M> {
     pub(crate) fn new() -> Self {
         Self(HashMap::new())
     }
     /// Inserts a message and returns the amount of so far collected
     /// messages with the same key as the given message.
-    pub(crate) fn collect(&mut self, msg: M, from: ReplicaId, key: K, config: &Config) -> u64 {
-        let index = from.as_u64() as usize;
-        let counter;
+    fn collect(&mut self, msg: M, from: ReplicaId, key: K) -> u64 {
         match self.0.get_mut(&key) {
-            Some((actual_counter, messages)) => {
+            Some(messages) => {
                 // At least one message with the same key has been received before.
-                if messages[index].is_some() {
+                if messages.get(&from).is_some() {
                     debug!("Skipped inserting message (origin: {from:?}) into collector: Message was a duplicate.");
-                    return *actual_counter;
+                    return messages.len() as u64;
                 }
                 // The given message is new.
-                let mut messages = mem::take(messages);
-                messages[index] = Some(msg);
-                counter = *actual_counter + 1;
-                self.0.insert(key, (counter, messages));
+                messages.insert(from, msg);
                 debug!("Inserted message (origin: {from:?}) into collector.");
             }
             None => {
-                // No message with the same key has been received before.
-                let mut messages = Vec::with_capacity(config.n.get() as usize);
-                let mut amount_initialized = 0;
-                while amount_initialized < config.n.get() {
-                    messages.push(None);
-                    amount_initialized += 1;
-                }
-                messages[index] = Some(msg);
-                counter = 1;
-                self.0.insert(key, (counter, messages));
+                // No message with the same key has been received before
+                let mut messages = HashMap::new();
+                messages.insert(from, msg);
+                self.0.insert(key.clone(), messages);
                 debug!("Inserted message (origin: {from:?}) into collector.");
             }
         }
-        counter
+        self.0.get_mut(&key).unwrap().len() as u64
     }
 
     /// Retrieves a collection of at least t + 1 messages if they are valid and
     /// if already at least t + 1 messages have been received with the same given key.
     /// One of the messages must have been broadcast by the replica itself.
     /// If this is the case, then the collection only retains messages which have a higher key.
-    pub(crate) fn retrieve(&mut self, key: K, config: &Config) -> Option<(M, Vec<M>)> {
-        let collected = self.0.get(&key);
-        let collected = collected?;
-        if collected.0 <= config.t {
+    fn retrieve(&mut self, key: K, config: &Config) -> Option<(M, Vec<M>)> {
+        let messages = self.0.get_mut(&key);
+
+        // Check if at least `t + 1` messages have been received.
+        messages.as_ref()?;
+        let messages = messages.unwrap();
+        if messages.len() <= config.t.try_into().unwrap() {
             return None;
         }
-        // At least t + 1 received.
-        if collected
-            .1
-            .get(config.id.as_u64() as usize)
-            .unwrap()
-            .is_none()
-        {
+
+        // Check if the replica's own message has been received.
+        if !messages.contains_key(&config.id) {
             return None;
         }
+
         // The replica's own message has been received, too.
         // Collect messages and clean up collector.
         let mut my_retrieved_message = None;
         let mut other_retrieved_messages = Vec::new();
-        let (_, mut messages) = self.0.remove(&key).unwrap();
-        for (from, msg) in messages.iter_mut().enumerate() {
-            if let Some(msg) = msg.take() {
-                if config.id.as_u64() == from as u64 {
-                    my_retrieved_message = Some(msg);
-                } else {
-                    other_retrieved_messages.push(msg);
-                }
+        let mut messages = self.0.remove(&key).unwrap();
+
+        for (rep_id, msg) in messages.drain() {
+            if rep_id == config.id {
+                my_retrieved_message = Some(msg);
+            } else {
+                other_retrieved_messages.push(msg);
             }
         }
+
         self.0.retain(|n, _| n > &key);
         let my_retrieved_message = my_retrieved_message?;
         Some((my_retrieved_message, other_retrieved_messages))
